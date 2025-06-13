2c0d2e07d1a258106a89f9a3b03ecf161a80246b
WCYuan <2750710933@qq.com>
2025-06-13 09:40:05 +0800

    add imgs, fix an error in Panel, add a title

--- src/Panel.java
+++ src/Panel.java
@@ -1,15 +1,17 @@
 import java.awt.Color;
 import java.awt.Graphics;
 import javax.swing.JPanel;
+import javax.swing.ImageIcon;
 
 public class Panel extends JPanel{
-    
+    ImageIcon title = new ImageIcon("resource/img/title.jpg");
     public Panel() {
 
     }
 
     public void paintComponent(Graphics g) {
-        super.paint(g);
-        this.setBackground(Color.GREEN);
+        super.paintComponent(g);
+        this.setBackground(Color.WHITE);
+        title.paintIcon(this, g, 25, 11);
     }
 }
