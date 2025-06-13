15cd5977da1e4fbb544c7c018507633306271377
WCYuan <2750710933@qq.com>
2025-06-13 10:10:21 +0800

    put a static snake on panel

--- src/Panel.java
+++ src/Panel.java
@@ -5,6 +5,13 @@ import javax.swing.ImageIcon;
 
 public class Panel extends JPanel{
     ImageIcon title = new ImageIcon("resource/img/title.jpg");
+    ImageIcon body = new ImageIcon("resource/img/body.png");
+    ImageIcon up = new ImageIcon("resource/img/up.png");
+    ImageIcon down = new ImageIcon("resource/img/down.png");
+    ImageIcon left = new ImageIcon("resource/img/left.png");
+    ImageIcon right = new ImageIcon("resource/img/right.png");
+    ImageIcon food = new ImageIcon("resource/img/food.png");
+
     public Panel() {
 
     }
@@ -13,5 +20,10 @@ public class Panel extends JPanel{
         super.paintComponent(g);
         this.setBackground(Color.WHITE);
         title.paintIcon(this, g, 25, 11);
+
+        g.fillRect(25, 75, 25*34, 25*24);
+        right.paintIcon(this, g, 100, 100);
+        body.paintIcon(this, g, 75, 100);
+        body.paintIcon(this, g, 50, 100);
     }
 }
